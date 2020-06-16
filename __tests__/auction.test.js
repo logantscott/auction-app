45dc6b32b5bab46e2abb1a3694189c7fc5cfdb7b
Logan Scott <logantscott@gmail.com>
2020-06-16 10:38:15 -0600

    can create auction test passes

--- __tests__/auction.test.js
+++ __tests__/auction.test.js
@@ -5,6 +5,7 @@ const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
 describe('auction-app routes', () => {
   beforeAll(async() => {
@@ -16,13 +17,40 @@ describe('auction-app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let user;
+  beforeEach(async() => {
+    user = await User
+      .create({
+        email: 'logan@test.com',
+        password: '1234'
+      })
+      .then(res => res);
+  });
+
   afterAll(async() => {
     await mongoose.connection.close();
     return mongod.stop();
   });
   
   it('can create an auction', () => {
-    return expect('hi').toBe('hi');
+    return request(app)
+      .post('/api/v1/auctions')
+      .send({
+        user: user.id,
+        title: 'my first auction',
+        description: 'some boring thing being sold',
+        quantity: 2,
+        endDate: Date.now()
+      })
+      .then(res => expect(res.body).toEqual({
+        _id: expect.anything(),
+        user: user.id,
+        title: 'my first auction',
+        description: 'some boring thing being sold',
+        quantity: 2,
+        endDate: expect.anything(),
+        __v: 0
+      }));
   });
 
   it('can get an auction by id', () => {
